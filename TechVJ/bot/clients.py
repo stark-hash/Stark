1f5846bc2069d06a0522afc1be13bb6eac4908f0
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-05-28 08:00:42 +0530

    Update clients.py

--- TechVJ/bot/clients.py
+++ TechVJ/bot/clients.py
@@ -7,11 +7,11 @@ import logging
 from info import *
 from pyrogram import Client
 from TechVJ.util.config_parser import TokenParser
-from TechVJ.bot import multi_clients, work_loads, TechVJBot
+from TechVJ.bot import multi_clients, work_loads, Bot
 
 
 async def initialize_clients():
-    multi_clients[0] = TechVJBot
+    multi_clients[0] = Bot
     work_loads[0] = 0
     all_tokens = TokenParser().parse_from_env()
     if not all_tokens:
