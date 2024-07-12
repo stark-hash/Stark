c5514185bddde0dca886d7cb19bf052b613c9eb3
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-07-12 20:51:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ class Bot(Client):
         await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
-        temp.U_NAME = me.username
+        temp.U_NAME = AnAutoFilterBot
         temp.B_NAME = me.first_name
         self.id = me.id
         self.name = me.first_name
