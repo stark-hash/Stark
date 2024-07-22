c1af813d3599aa828435014d43982617d3c00ae6
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-07-22 15:34:55 +0530

    ok

--- CloneTechVJ/commands.py
+++ CloneTechVJ/commands.py
@@ -4,16 +4,17 @@
 
 # Clone Code Credit : YT - @Tech_VJ / TG - @VJ_Bots / GitHub - @VJBots
 
-import logging, asyncio, base64
+import os, string, logging, random, asyncio, time, datetime, re, sys, json, base64
 from Script import script
 from pyrogram import Client, filters, enums
+from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import *
-from database.ia_filterdb import Media, get_file_details
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
 from CloneTechVJ.database.clone_bot_userdb import clonedb
 from info import *
 from shortzy import Shortzy
-from utils import get_size, temp, get_clone_shortlink
+from utils import get_size, temp, get_seconds, get_clone_shortlink
 logger = logging.getLogger(__name__)
 
 @Client.on_message(filters.command("start") & filters.incoming)
