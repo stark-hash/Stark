c5be9b2f87b5caecd043103ff73d7b2dcb8a6339
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-07-22 07:41:57 +0530

    ok

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -8,6 +8,7 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
+        self.bot = self.db.clone_bots
 
 
     def new_user(self, id, name):
