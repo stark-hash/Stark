ef87089731caa00ace2e99a4f84ef9801cbc1e0f
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-09-26 16:43:39 +0400

    Update info.py

--- info.py
+++ info.py
@@ -71,6 +71,14 @@ QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
 YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]
 
 
+
+CARDPICS = (environ.get('CARDPICS' ,'https://envs.sh/S0R.jpg')).split()
+YTPICS = (environ.get('CARDPICS' ,'https://envs.sh/S05.jpg')).split()
+SPOTIPICS = (environ.get('CARDPICS' ,'https://envs.sh/S0K.jpg')).split()
+PINTEPICS = (environ.get('CARDPICS' ,'https://envs.sh/S0Y.jpg')).split()
+INSTAPICS = (environ.get('CARDPICS' ,'https://envs.sh/S04.jpg')).split()
+
+
 # Admins, Channels & Users
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
