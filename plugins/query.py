3128f5b178f21b12cda07657bdaa17443683b0b0
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-11-12 08:15:40 +0400

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -13,7 +13,7 @@ from .ExtraMods.carbon import make_carbon
 
 # Database Function 
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
-from database.ia_filterdb import Media, get_file_details, get_search_results
+from database.ia_filterdb import Media, get_file_details, get_search_results, Media2
 from database.filters_mdb import del_all, find_filter, get_filters
 from database.gfilters_mdb import find_gfilter, get_gfilters
 from database.users_chats_db import db
@@ -813,9 +813,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
         monsize = await db.get_db_size()
         free = 536870912 - monsize
         monsize = get_size(monsize)
+        secdb = await Media2.count_documents()
         free = get_size(free)
         await query.message.edit('ʟᴏᴀᴅɪɴɢ....')
-        await query.edit_message_media(InputMediaPhoto(random.choice(PICS), script.STATUS_TXT.format(total, users, chats, monsize, free), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))
+        await query.edit_message_media(InputMediaPhoto(random.choice(PICS), script.STATUS_TXT.format(total, secdb, users, chats, monsize, free), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))
     
     elif query.data.startswith("setgs"):
         ident, set_type, status, grp_id = query.data.split("#")
