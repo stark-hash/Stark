2dec81a9723e45818207ba6e44883544dd0876d7
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-12-04 00:09:27 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,6 +39,8 @@ class Bot(Client):
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats    
         await super().start()
+        await Media.ensure_indexes()
+        await Media2.ensure_indexes()
         #choose the right db by checking the free space
         stats = await clientDB.command('dbStats')
         #calculating the free db space from bytes to MB
